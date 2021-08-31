9227b390da9e7d951c382185dc713ed402be89bf
Deano Calver <me@deanoc.com>
2021-08-31 10:51:03 +0300

    c++

--- aarch64_toolchain.cmake
+++ aarch64_toolchain.cmake
@@ -18,6 +18,7 @@ set(CMAKE_C_COMPILER ${COMPILER_PATH}/bin/aarch64-none-elf-gcc)
 set(CMAKE_C_COMPILER_AR ${COMPILER_PATH}/bin/aarch64-none-elf-ar)
 set(CMAKE_C_COMPILER_RANLIB ${COMPILER_PATH}/bin/aarch64-none-elf-ranlib)
 
+
 set(CMAKE_LINKER ${COMPILER_PATH}/bin/aarch64-none-elf-ld)
 
 # TODO 
@@ -29,12 +30,22 @@ set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} ${CPU_SETTINGS}")
 
 set(CMAKE_C_STANDARD_INCLUDE_DIRECTORIES
 			${COMPILER_PATH}/lib/gcc/aarch64-none-elf/${GCC_VERSION}/include
-      ${COMPILER_PATH}/lib/gcc/aarch64-none-elf/${GCC_VERSION}/include-fixed)
+      		${COMPILER_PATH}/lib/gcc/aarch64-none-elf/${GCC_VERSION}/include-fixed)
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -ggdb")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -flto -ffat-lto-objects")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -nostartfiles -nostdlib -nostdinc -ffreestanding")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wpedantic -Wno-builtin-declaration-mismatch")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${CPU_SETTINGS} -std=gnu2x")
+
+set(CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES
+		${COMPILER_PATH}/lib/gcc/aarch64-none-elf/${GCC_VERSION}/include
+		${COMPILER_PATH}/lib/gcc/aarch64-none-elf/${GCC_VERSION}/include-fixed)
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ggdb")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -flto -ffat-lto-objects")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -nostartfiles -nostdlib -nostdinc -ffreestanding -fno-rtti -fno-exceptions")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wpedantic -Wno-builtin-declaration-mismatch")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CPU_SETTINGS} -std=gnu++20")
 
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -nostartfiles -nostdlib -nostdinc -lgcc")
